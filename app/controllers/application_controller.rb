700ce8b0201221e2ffbe3f1a1841acdeaf277a32
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-03-18 00:33:33 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,7 +43,7 @@ class ApplicationController < Sinatra::Base
     @recipe = Recipe.find_by_id(params[:id])
     #@recipe = Recipe.find_by_id(params[:id])
     @recipe.delete
-    redirect to '/recipes'
+    redirect '/recipes'
   end
 
 
