ceffee4008865973eb72c08118b62a1658ba323b
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-03-18 00:36:24 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,7 +26,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/recipes/:id/edit' do
-    @article = Article.find_by_id(params[:id])
+    @article = Recipe.find_by_id(params[:id])
     erb :edit
   end
 
